7af4540f6588a6850e881e74eb2c96abd2b4d5e6
Abdallah Zahran <abdallahzahran29@gmail.com>
2024-10-15 02:46:16 +0300

    edited

---
+++ Assets/Scripts/SpaceshipsController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipsController : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] private float speed;
+    private float horizonal;
+
+
+    [Header("Clamp")]
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        horizonal = Input.GetAxis("Horizontal") * Time.deltaTime;
+       
+        
+        if (horizonal > 0)
+        {
+            transform.position += Vector3.right * speed;
+        }
+        else if(horizonal < 0)
+        {
+            transform.position -= Vector3.left * -speed;
+        }
+
+     
+        // Mathf.Clamp
+        float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+        
+       
+
+       
+
+    }
+
+   
+
+}
